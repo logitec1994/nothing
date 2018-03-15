61dd31b12f116584573cda0bbd794b71fe9eb66c
logitec1994 <123654978a@gmail.com>
2018-03-15 21:13:04 +0200

    Strings

---
+++ StringExamples/SimpleStr/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    char str[20] = {'S', 'o', 'm', 'e', ' ', 'S', 't', 'r', 'i', 'n', 'g', '\0'};
+
+    std::cout << str << "\n";
+
+    return 0;
+}
